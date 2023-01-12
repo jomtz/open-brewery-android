06d8edd2e84bd2a47cc6b79908d014c1ecdd1ee6
josmtz <46319076+josmtz@users.noreply.github.com>
2023-01-12 13:37:13 -0500

    Add ScrollView and DataBinding

--- app/src/main/java/com/josmtz/openbrewery/ui/home/HomeFragment.kt
+++ app/src/main/java/com/josmtz/openbrewery/ui/home/HomeFragment.kt
@@ -1,33 +1,61 @@
 package com.josmtz.openbrewery.ui.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.josmtz.openbrewery.R
+import com.josmtz.openbrewery.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
+    private lateinit var wordList: MutableList<String>
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        )
+
+        breweryList()
+
+        return binding.root
     }
 
-    private lateinit var viewModel: HomeViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private fun breweryList() {
+        // All the breweries for this List
+        wordList = mutableListOf<String>(
+            "10-56 Brewing Company",
+            "10 Barrel Brewing Co",
+            "10 Barrel Brewing Co - Bend Pub",
+            "10 Barrel Brewing Co - Boise",
+            "10 Barrel Brewing Co - Denver",
+            "10 Torr Distilling and Brewing",
+            "101 Brewery",
+            "101 North Brewing Company",
+            "105 West Brewing Co",
+            "10K Brewing",
+            "10th District Brewing Company",
+            "11 Below Brewing Company",
+            "1188 Brewing Co",
+            "12 Acres Brewing Company",
+            "12 Gates Brewing Company",
+            "12 West Brewing Company",
+            "12 West Brewing Company - Production Facility"
+        )
+        binding.textview.text = wordList.toString()
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
+
+
+
+
