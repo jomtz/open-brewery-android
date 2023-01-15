cc11f93b410e01655a018fc906b441fa8e94d82d
josmtz <46319076+josmtz@users.noreply.github.com>
2023-01-14 21:55:29 -0500

    Add navHostFragment

--- app/src/main/java/com/josmtz/openbrewery/MainActivity.kt
+++ app/src/main/java/com/josmtz/openbrewery/MainActivity.kt
@@ -2,10 +2,20 @@ package com.josmtz.openbrewery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
     }
 }
