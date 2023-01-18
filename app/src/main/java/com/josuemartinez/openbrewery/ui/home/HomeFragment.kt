56763f9628e646a538b78a9aa15a6a8806257d01
josuemnez <46319076+josuemnez@users.noreply.github.com>
2023-01-18 13:03:14 -0500

    Add HomeViewModel with List

--- app/src/main/java/com/josuemartinez/openbrewery/ui/home/HomeFragment.kt
+++ app/src/main/java/com/josuemartinez/openbrewery/ui/home/HomeFragment.kt
@@ -6,12 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.josuemartinez.openbrewery.R
 import com.josuemartinez.openbrewery.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var wordList: MutableList<String>
+    private lateinit var viewModel: HomeViewModel
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -24,34 +25,20 @@ class HomeFragment : Fragment() {
             false
         )
 
-        breweryList()
+
+
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        listOfWords()
 
         return binding.root
     }
 
-    private fun breweryList() {
-        // All the breweries for this List
-        wordList = mutableListOf<String>(
-            "10-56 Brewing Company",
-            "10 Barrel Brewing Co",
-            "10 Barrel Brewing Co - Bend Pub",
-            "10 Barrel Brewing Co - Boise",
-            "10 Barrel Brewing Co - Denver",
-            "10 Torr Distilling and Brewing",
-            "101 Brewery",
-            "101 North Brewing Company",
-            "105 West Brewing Co",
-            "10K Brewing",
-            "10th District Brewing Company",
-            "11 Below Brewing Company",
-            "1188 Brewing Co",
-            "12 Acres Brewing Company",
-            "12 Gates Brewing Company",
-            "12 West Brewing Company",
-            "12 West Brewing Company - Production Facility"
-        )
-        binding.textview.text = wordList.toString()
+    private fun listOfWords(){
+        binding.textview.text = viewModel.wordList.toString()
     }
+
+
 
 
 }
--- app/src/main/java/com/josuemartinez/openbrewery/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/josuemartinez/openbrewery/ui/home/HomeViewModel.kt
@@ -1,7 +1,42 @@
 package com.josuemartinez.openbrewery.ui.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
+    lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel Created!" )
+
+
+        breweryList()
+
+    }
+
+    fun breweryList() {
+        // All the breweries for this List
+        wordList = mutableListOf<String>(
+            "10-56 Brewing Company",
+            "10 Barrel Brewing Co",
+            "10 Barrel Brewing Co - Bend Pub",
+            "10 Barrel Brewing Co - Boise",
+            "10 Barrel Brewing Co - Denver",
+            "10 Torr Distilling and Brewing",
+            "101 Brewery",
+            "101 North Brewing Company",
+            "105 West Brewing Co",
+            "10K Brewing",
+            "10th District Brewing Company",
+            "11 Below Brewing Company",
+            "1188 Brewing Co",
+            "12 Acres Brewing Company",
+            "12 Gates Brewing Company",
+            "12 West Brewing Company",
+            "12 West Brewing Company - Production Facility"
+        )
+
+    }
 }
