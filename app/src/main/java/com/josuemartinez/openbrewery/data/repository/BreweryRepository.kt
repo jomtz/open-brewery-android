c2a1350653d7ae2c3837cd687045dfe29ffdb505
josuemnez <46319076+josuemnez@users.noreply.github.com>
2023-01-20 23:21:31 -0500

    Create BreweryRepository.kt

---
+++ app/src/main/java/com/josuemartinez/openbrewery/data/repository/BreweryRepository.kt
@@ -0,0 +1,23 @@
+package com.josuemartinez.openbrewery.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class BreweryRepository {
+
+    /**
+     * A list of breweries that can be shown on the screen.
+     */
+
+    /** TODO: */
+
+    /**
+     * Refresh the breweries stored in the offline cache.
+     * To actually load the breweries for use, observe [breweries]
+     */
+
+    /** TODO: */
+
+}
