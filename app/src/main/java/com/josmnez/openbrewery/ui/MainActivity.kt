b268d17c8b77ec0c329e942f7d1ef2e9cf87c22e
josmnez <46319076+josmnez@users.noreply.github.com>
2023-01-30 16:39:15 -0500

    Rename package on Gradle

--- app/src/main/java/com/josmnez/openbrewery/ui/MainActivity.kt
+++ app/src/main/java/com/josmnez/openbrewery/ui/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.josuemartinez.openbrewery.R
+import com.josmnez.openbrewery.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
