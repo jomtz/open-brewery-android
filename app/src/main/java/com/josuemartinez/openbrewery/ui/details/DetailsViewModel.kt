03a3d51f698c63d6f3f33cbdab06f780d74ad6fa
josuemnez <46319076+josuemnez@users.noreply.github.com>
2023-02-07 15:20:40 -0500

    Create DetailsViewModel.kt

---
+++ app/src/main/java/com/josuemartinez/openbrewery/ui/details/DetailsViewModel.kt
@@ -0,0 +1,40 @@
+package com.josuemartinez.openbrewery.ui.details
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.josuemartinez.openbrewery.data.database.BreweryDatabase
+import com.josuemartinez.openbrewery.data.repository.BreweryRepository
+
+class DetailsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = BreweryDatabase.getDatabase(application)
+    private val breweryRepository = BreweryRepository(database)
+
+
+
+    private val breweryId = MutableLiveData<String>()
+    val brewery = breweryId.switchMap { id ->
+        liveData {
+            emitSource(breweryRepository.getBrewery(id))
+        }
+    }
+
+    fun getBrewery(id: Int) {
+        breweryId.value = id.toString()
+    }
+
+
+
+
+    class DetailsViewModelFactory(val app: Application): ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DetailsViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+
+    }
+
+}
